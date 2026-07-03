97e3c0d88a19d9b49b8cb252b8be1084b46d3dd9
Alireza <alireza.nourasli@gmail.com>
2026-07-03 21:39:05 +0330

    feat(card)edit border color property in EmptystateComponent

--- src/components/EmptyStateComponent.ts
+++ src/components/EmptyStateComponent.ts
@@ -3,7 +3,7 @@ export function EmptyComponent() {
         <!-- EmptyStateComponent -->
       <div
         id="Card"
-        class="h-14 w-full max-w-80 md:max-w-186 p-4 border border-[#CCCCCC] rounded-[10px] border-dashed"
+        class="h-14 w-full p-4 border border-border rounded-[10px] border-dashed"
       >
         <button class="flex text-primary font-semibold text-sm gap-2">
           <img class="w-6 h-6" src="./src/assets/add.png" alt="Plus-Pic" />
