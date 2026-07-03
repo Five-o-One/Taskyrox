026405dccd97376f50ccbb4c5abb0e48305be43c
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-07-03 21:14:54 +0330

    fix(navbar_mainitem):add type for navbar items , add dark mode for icons.

--- src/components/NavBar/navbar_mainitem.ts
+++ src/components/NavBar/navbar_mainitem.ts
@@ -1,14 +1,14 @@
-export function NavBarMainItem(
-  itemId: string,
-  itemIconDefault: string,
-  itemIconActive: string,
-  itemLabel: string,
-  isActive: boolean,
-): string {
+import type { NavBarItem } from "../../types/navbar_item";
+export function NavBarMainItem({
+  itemId,
+  isActive,
+  itemIcon,
+  itemLabel,
+}: NavBarItem): string {
   return `
-  <li data-nav-id="${itemId}" class="w-full flex flex-row items-center gap-4 py-2 pr-1  rounded-lg ${isActive ? "bg-primary-soft dark:bg-primary-soft" : ""}">
-    <img src="${isActive ? itemIconActive : itemIconDefault}" alt="${itemLabel}" class="size-5 object-contain"/>
-    <span class="text-sm  dark:text-text ${isActive ? "text-primary-hover font-bold" : "text-text-muted font-semibold"}">${itemLabel}</span>
-  </li>
+  <div id="${itemId}" class="w-full flex flex-row items-center gap-4 py-2 pr-1  rounded-lg ${isActive ? "bg-primary-soft" : ""}">
+    <svg src="${itemIcon}" alt="${itemLabel}" class="size-5 object-contain dark:fill-text ${isActive ? "fill-primary" : "fill-text-muted"}" />
+    <span class="text-sm ${isActive ? "text-primary-hover font-bold" : "text-text-muted font-semibold"}">${itemLabel}</span>
+  </div>
   `;
 }
