27a78f8d27a6eaa40212389f0a29955fe6be12f4
Alireza <alireza.nourasli@gmail.com>
2026-07-03 20:31:45 +0330

    feat(card):add card component

--- src/components/TaskCardComponent.ts
+++ src/components/TaskCardComponent.ts
@@ -1,44 +1,70 @@
-export function TaskCard(
-  bgColor: string,
-  txtColor: string,
-  Titletxt: string,
-  DescTxt: string,
-) {
-  return `
-  <!-- TaskCardComponent -->
+import type { Task } from "../types/task";
+import { PriorityNavColor } from "./PriorityNavColor";
+import { ColorPriorityPiece } from "./PriortiySpanColor";
+
+export function TaskCard(task: Task) {
+  if (task.isDone) {
+    return `
+    <!-- TaskCardComponent -->
       <div
-        class="relative w-full max-w-80 md:max-w-186 border border-borderColor py-3 px-4 rounded-xl overflow-hidden"
+        class="relative w-full h-fit border border-border py-3 px-4 rounded-xl overflow-hidden"
       >
-        <div
-          id="priorityColorID"
-          class="absolute top-0 right-0 bg-${bgColor} h-full w-1 rounded-lg"
-        ></div>
+        ${PriorityNavColor(task.priority)}
         <!-- checkbox and title ردیف بالا-->
         <div class="flex justify-between items-start">
           <!-- right -->
           <div class="flex items-start gap-4">
-            <input class="border border-borderColor w-5 h-5" type="checkbox" />
-            <div class="flex flex-col md:flex-row gap-1 md:gap-3">
-              <h3 class="text-sm font-YekanBakh font-semibold mb-1">
-              ${Titletxt}
+          <input class="border border-border w-5 h-5" type="checkbox" />
+          <div class="flex flex-col md:flex-row gap-1 md:gap-3">
+              <h3 class="text-sm font-semibold mb-1">
+              ${task.title}
               </h3>
-              <span
-                class="bg-${txtColor} items-center text-[10px] font-YekanBakh font-semibold w-fit text-${bgColor} py-0.5 px-2 rounded-sm"
-                >بالا</span
+              ${ColorPriorityPiece(task.priority)}
+              </div>
+              </div>
+              <!-- button left -->
+              <button>
+              <img class="rotate-90" src="../src/assets/icons/menu-light.svg" alt="more-Option" />
+              </button>
+        </div>
+        <!-- description -->
+        <div class="pr-8 mt-4">
+          <p class="text-text-secondary font-normal text-xs">
+          ${task.description}
+          </p>
+        </div>
+      </div>
+      `;
+  } else {
+    return `
+          <div
+        class="relative w-full h-fit border border-borderColor py-3 px-4 rounded-xl overflow-hidden"
+      >
+        ${PriorityNavColor(task.priority)}
+        <!-- checkbox and title ردیف بالا-->
+        <div class="flex justify-between items-start">
+          <!-- right -->
+          <div class="flex items-center gap-4">
+            <input
+              checked
+              class="border border-border w-5 h-5"
+              type="checkbox"
+            />
+            <div class="flex flex-col md:flex-row gap-1 md:gap-3">
+              <h3
+                class="text-sm line-through font-semibold mb-1"
               >
+                تماس با جمشید
+              </h3>
             </div>
           </div>
           <!-- button left -->
           <button>
-            <img src="./src/assets/Frame 33317.svg" alt="more-Option" />
+            <img class="rotate-90" src="./src/assets/icons/menu-light.svg" alt="more-Option" />
           </button>
         </div>
-        <!-- description -->
-        <div class="pr-8 mt-4">
-          <p class="text-gray-500 font-YekanBakh font-normal text-xs">
-          ${DescTxt}
-          </p>
-        </div>
       </div>
+    </div>
     `;
+  }
 }
--- src/components/main_content.ts
+++ src/components/main_content.ts
@@ -1,4 +1,14 @@
+import type { Task } from "../types/task";
+import { TaskCard } from "./TaskCardComponent";
+
 export function MainContent() {
+  const task: Task = {
+    description: "سلام",
+    isDone: false,
+    id: 1,
+    title: "شبنختیشخهبت",
+    priority: "MEDIUM",
+  };
   return `
-    <div class="w-full h-screen p-4 bg-primary md:w-4/5">سلام تست</div>`;
+    <div class="w-full h-screen p-4 bg-primary md:w-4/5">${TaskCard(task)}</div>`;
 }
