06a779a24817db5de9e7b55a519cf565ef453c97
Alireza <alireza.nourasli@gmail.com>
2026-07-03 22:10:04 +0330

    feat(card):add bgColor to Empty and task component and remove space from span in empty

--- src/components/EmptyStateComponent.ts
+++ src/components/EmptyStateComponent.ts
@@ -3,11 +3,11 @@ export function EmptyComponent() {
         <!-- EmptyStateComponent -->
       <div
         id="Card"
-        class="h-14 w-full p-4 border border-border rounded-[10px] border-dashed"
+        class="h-14 w-full p-4 border border-border bg-bg rounded-[10px] border-dashed"
       >
         <button class="flex text-primary font-semibold text-sm gap-2">
-          <img class="w-6 h-6" src="./src/assets/add.png" alt="Plus-Pic" />
-          <span class="font-YekanBakh"> افزودن وظیفه جدید </span>
+          <img class="w-6 h-6" src="../src/assets/" alt="Add-Pic" />
+          <span>افزودن وظیفه جدید</span>
         </button>
       </div>
     `;
