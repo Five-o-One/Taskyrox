d71325eef80f4ab71d249e4b9708f441ee70f1ca
Shahryar Pi <dev.shahryar@gmail.com>
2026-07-04 15:50:41 +0330

    feat(listTask): implement ListTask component for displaying tasks based on completion status feat(main_content): integrate ListTask component and update layout with test data feat(task): add TestDic for task-related text constants

--- src/components/main_content.ts
+++ src/components/main_content.ts
@@ -1,14 +1,20 @@
 import type { Task } from "../types/task";
-import { TaskCard } from "./TaskCardComponent";
+import { ListTask } from "./listTask";
+
+//! test data
+const testData : Task[] = [
+  { id: 1, title: "Task 1", description: "First test task", isDone: false, priority: "HIGH" },
+  { id: 2, title: "Task 2", description: "Second test task", isDone: false, priority: "MEDIUM" },
+  { id: 3, title: "Task 3", description: "Third test task", isDone: false, priority: "LOW" },
+  { id: 4, title: "Task 4", description: "Fourth test task", isDone: true, priority: "HIGH" },
+  { id: 5, title: "Task 5", description: "Fifth test task", isDone: true, priority: "MEDIUM" }
+]
+//! test data end
 
 export function MainContent() {
-  const task: Task = {
-    description: "سلام",
-    isDone: false,
-    id: 1,
-    title: "شبنختیشخهبت",
-    priority: "MEDIUM",
-  };
   return `
-    <div class="w-full h-screen p-4 bg-primary md:w-4/5">${TaskCard(task)}</div>`;
+    <div class="flex flex-col w-full h-screen p-4 bg-bg md:w-4/5">
+    <div class="w-full h-full p-4 overflow-auto">${ListTask(false , testData)}</div>
+    <div class="w-full h-full p-4 overflow-auto">${ListTask(true , testData)}</div>
+    </div>`;
 }
