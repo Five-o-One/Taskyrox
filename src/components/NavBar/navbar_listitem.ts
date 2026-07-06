25bf2ffdee9489a7639eb3cfdb87979ca6ff5339
Shahryar Pi <dev.shahryar@gmail.com>
2026-07-06 14:15:02 +0330

    fix (Navbar) : fix report icon

--- src/components/NavBar/navbar_listitem.ts
+++ src/components/NavBar/navbar_listitem.ts
@@ -4,7 +4,7 @@ const items: NavBarItem[] = [
   { itemIcon: "home", itemId: 0, itemLabel: "خانه" },
   { itemIcon: "checklist", itemId: 1, itemLabel: "تسک ها" },
   { itemIcon: "calendar", itemId: 2, itemLabel: "تقویم" },
-  { itemIcon: "info", itemId: 3, itemLabel: "گزارشات" },
+  { itemIcon: "square", itemId: 3, itemLabel: "گزارشات" },
   { itemIcon: "settings", itemId: 4, itemLabel: "تنظیمات" },
   { itemIcon: "info", itemId: 5, itemLabel: "پشتیبانی" },
   { itemIcon: "user", itemId: 6, itemLabel: "پروفایل" },
