444ccd426b5f5fb710e38f681fba38451e7e5a40
Alireza <alireza.nourasli@gmail.com>
2026-07-06 14:00:15 +0330

    state(card): create taskState and types of it

--- src/state/task_state.ts
+++ src/state/task_state.ts
@@ -1,1 +1,13 @@
-export
+import type { TaskState } from "../types/task_state_d";
+
+export const taskState: TaskState = {
+  tasks: [],
+  isDone: true,
+  isbtndisabled: true,
+  istagselected: false,
+  isoptionselected: false,
+  iseditbtnselected: true,
+  priorityselected: "HIGH",
+  descEdit: "",
+  titleEdit: "",
+};
--- src/types/task_state_d.ts
+++ src/types/task_state_d.ts
@@ -1,6 +1,13 @@
-import type { Task } from "./task";
+import type { priority, Task } from "./task";
 
 export interface TaskState {
   tasks: Task[];
-  
+  isDone: boolean;
+  istagselected: boolean;
+  isoptionselected: boolean;
+  iseditbtnselected: boolean;
+  isbtndisabled: boolean;
+  titleEdit: string;
+  descEdit: string;
+  priorityselected: priority;
 }
