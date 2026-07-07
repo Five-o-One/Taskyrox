3d6d4df5233697d417c7ede068098395738049ad
Shahryar <dev.shahryar@gmail.com>
2026-07-07 13:11:36 +0330

    feat(mainContent) : add an image for empty task undone section.

--- src/components/listTask.ts
+++ src/components/listTask.ts
@@ -2,29 +2,40 @@ import { TestDic } from "../dic/task";
 import type { Task } from "../types/task";
 import { AddNewTask } from "./Card/add_task";
 import { Card } from "./Card/card";
-export function ListTask(isDone:boolean , list: Task[]) {
-    const tasks : Task[] = list.filter(t=>!t.isDone)
-    const tasksDone : Task[] = list.filter(t=>t.isDone)
-    if(!isDone){
-            return `<div class="flex flex-col w-full h-full">
+
+const EmptyPage = () => `
+<div class="w-full h-full flex flex-col justify-center items-center gap-4">
+<img src="/public/images/empty.svg" alt="empty">
+<div class="flex flex-col text-center justify-center items-center gap-4">
+<span class="text-text-secondary text-md">چه کارهایی امروز برای انجام داری؟</span>
+<span class="text-text-muted text-sm">میتونی الان تسک‌هاتو اینجا بنویسی و برنامه ریزی رو شروع کنی!</span></div>
+</div>
+`;
+
+export function ListTask(isDone: boolean, list: Task[]) {
+  const tasks: Task[] = list.filter((t) => !t.isDone);
+  const tasksDone: Task[] = list.filter((t) => t.isDone);
+  if (!isDone) {
+    return `<div class="flex flex-col w-full h-full">
             <div class="flex flex-col mb-4 space-y-4">
             <span class="font-bold text-lg text-text">${TestDic.titleListNotDone}</span>
             <span class="text-sm text-text-secondary">${TestDic.subTittleNotDone(tasks.length)}</span>
             ${AddNewTask()}
             </div>
-            <div class="space-y-2 overflow-auto max-h-52">
-    ${tasks.map(task => `${Card("NotDone" , task)}`).join('')}
-            </div>
-            </div>`
-    }else{
-        return `<div class="flex flex-col w-full h-full">
+        
+    ${tasks.length !== 0 ? `<div class="space-y-2 overflow-auto max-h-52"> ${tasks.map((task) => Card("NotDone", task)).join("")} </div>` : EmptyPage()}
+            </div>`;
+  } else {
+    return tasksDone.length !== 0
+      ? `<div class="flex flex-col w-full h-full">
         <div class="flex flex-col mb-4 space-y-4">
         <span class="font-bold text-lg text-text">${TestDic.titleListDone}</span>
         <span class="text-sm text-text-secondary">${TestDic.subTittleDone(tasksDone.length)}</span>
         </div>
         <div class="space-y-2 overflow-auto max-h-52">
-    ${tasksDone.map(task => `${Card("Done" , task)}`).join('')}
+    ${tasksDone.map((task) => `${Card("Done", task)}`).join("")}
         </div>
         </div>`
-    }
+      : "";
+  }
 }
