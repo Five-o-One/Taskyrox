6d341dacea0695599a71ad6199acca5e911f6f95
Dorsa <dorize080@yahoo.com>
2026-07-07 17:20:27 +0330

    docs(card): update component documentation

--- src/components/Card/card.ts
+++ src/components/Card/card.ts
@@ -6,6 +6,13 @@ import { DoneCardComponent } from "./done_card";
 import { Modifycard } from "./modify_card";
 import { UnDoneCardComponent } from "./undone_card";
 
+/**
+ * This component selects the correct task card component based on the current card state.
+ * @param state States the current card state (Done, NotDone, or Edit).
+ * @param task The task information.
+ * @returns HTML string for the selected task card component.
+ */
+
 export function Card(
   state: TaskCardState,
   task: Task,
