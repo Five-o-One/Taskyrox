6e23e1a3adb3b368c430850628d736d04fe7275e
Shahryar Pi <dev.shahryar@gmail.com>
2026-07-07 22:49:49 +0330

    feat(theme): implement theme toggle functionality and update theme rendering

---
+++ src/core/theme_toggle.ts
@@ -0,0 +1,11 @@
+import { themeRender } from "../render/theme_render"
+import { appState } from "../state/app_state"
+
+export function themeToggle(){
+    appState.theme =
+        appState.theme === "dark"
+            ? "light"
+            : "dark"
+
+    themeRender()
+}
---
+++ src/render/theme_render.ts
@@ -0,0 +1,8 @@
+import { appState } from "../state/app_state";
+
+export function themeRender() {
+    const body = document.getElementById("body") as HTMLElement
+    if (body) {
+        body.setAttribute("data-theme", appState.theme)
+    }
+}
