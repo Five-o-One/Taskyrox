6c66e004921dd2ab50ef329dca83120dacb5742e
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-07-08 20:34:41 +0330

    feat(NavBar): update mobile header layout and add event handler

--- src/components/body.ts
+++ src/components/body.ts
@@ -1,8 +1,10 @@
 import { MainContent } from './main_content'
 import { NavBar } from './navbar'
+import { NavbarMobileHeader } from './NavBar/navbar_header'
 
 export function Body() {
   return /* HTML */ `
+    ${NavbarMobileHeader()}
     <main
       class="bg-bg container mx-auto flex h-screen w-full flex-row items-center justify-center overflow-hidden"
     >
