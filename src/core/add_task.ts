0202680c0b5fd24d559c8369d450fb82ef4f4c2d
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-07-08 23:13:22 +0330

    test

--- src/core/add_task.ts
+++ src/core/add_task.ts
@@ -1,11 +1,10 @@
 import { Modifycard } from '../components/Card/modify_card'
 import { currentTask } from '../state/current_task'
 
-
-
 export function AddTask(target: HTMLElement): boolean {
   const btn = target.closest('#NewTaskCard')
   if (!btn) return false
   btn.outerHTML = Modifycard(currentTask)
   return true
 }
+// hhhfishfkh
