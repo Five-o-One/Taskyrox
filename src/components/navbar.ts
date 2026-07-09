f5335f9e33cd7576f8a09281f3608ced7c7591b2
Dorsa <dorize080@yahoo.com>
2026-07-09 13:43:55 +0330

    docs(navbar): update component documentation

--- src/components/navbar.ts
+++ src/components/navbar.ts
@@ -2,6 +2,13 @@ import { NavBarItem } from './NavBar/navbar_item'
 import { NavBarList } from './NavBar/navbar_list'
 import { NavBarProfile } from './NavBar/navbar_profile'
 import { NavBarThemeToggle } from './NavBar/navbar_theme_toggle'
+
+/**
+ * Creates the main navigation sidebar.
+ * Displays the user profile, navigation list, theme toggle, and logout item.
+ * @returns Navigation bar HTML.
+ */
+
 export function NavBar() {
   return /* HTML */ `
     <navbar
