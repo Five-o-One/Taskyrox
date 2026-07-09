edd34ed737498fc3c2f743297490a8228c57adf4
Alireza <alireza.nourasli@gmail.com>
2026-07-09 18:58:10 +0330

    feat(card):close card and reset every property added, add all property in local storage

--- src/core/close_caard.ts
+++ src/core/close_caard.ts
@@ -1,8 +1,9 @@
 import { renderAddTaskCard } from '../render/add_task_render'
 import { currentTask } from '../state/current_task'
-
+import { resetCurrentTask } from './reset_current_task'
 
 export function CloseBtn(target: HTMLElement): void {
   currentTask.state.isOptionOpened = false
+  resetCurrentTask()
   renderAddTaskCard(target)
 }
--- src/core/priority.ts
+++ src/core/priority.ts
@@ -1,10 +1,12 @@
 import { renderModifyCard } from '../render/modify_render'
 import { currentTask } from '../state/current_task'
-import type { priority, Task } from '../types/task'
+import type { Task } from '../types/task'
+import { SaveTasks } from './task_storage'
 
 export function SelectPrBtn(priority: Task['priority']) {
   currentTask.priority = priority
   currentTask.state.isTagmenuOpened = false
+  SaveTasks(currentTask)
   renderModifyCard()
 }
 
