6f5118873cfc9419ada18baa0383a6c0ee990aad
Alireza <alireza.nourasli@gmail.com>
2026-07-10 10:07:56 +0330

    feat(card):conflict is ok now

--- src/components/Card/edit_btn_div.ts
+++ src/components/Card/edit_btn_div.ts
@@ -1,22 +1,15 @@
 import { iconGen } from '../../utils/icon_generator'
 
-export function EditButtonDiv(id: number) {
+export function EditButtonDiv() {
   return /* HTML */ `
     <div
-      data-key="option-${id}"
       class="border-border bg-bg flex flex-row gap-2.5 rounded-lg border p-1.5 shadow"
     >
-      <div
-        data-key="remove-${id}"
-        class="cursor-pointer select-none active:scale-90"
-      >
+      <div class="cursor-pointer select-none active:scale-90">
         ${iconGen('trash', 'fill-icon')}
       </div>
       <div class="bg-border h-5 w-px rounded-2xl"></div>
-      <div
-        data-key="edit-${id}"
-        class="cursor-pointer select-none active:scale-90"
-      >
+      <div class="cursor-pointer select-none active:scale-90">
         ${iconGen('pen', 'fill-icon')}
       </div>
     </div>
