007dc99a962167a6c9db98f90edb1b9ae7e4ed06
Dorsa <dorize080@yahoo.com>
2026-07-10 13:49:48 +0330

    refactor(navbar_profile): move hardcoded texts to dictionary

--- src/components/NavBar/navbar_profile.ts
+++ src/components/NavBar/navbar_profile.ts
@@ -1,3 +1,4 @@
+import { FaDic } from '../../dic/fa'
 import { appState } from '../../state/app_state'
 import { NavProfileAvatar } from './navbar_profile_avatar'
 
@@ -13,7 +14,7 @@ export function NavBarProfile(): string {
     <div class="flex max-w-58 flex-row gap-3">
       ${NavProfileAvatar(avatarUrl)}
       <div class="flex flex-col gap-1">
-        <p class="text-text text-lg font-extrabold">سلام ${username}</p>
+        <p class="text-text text-lg font-extrabold">${FaDic.navbarGreeting} ${username}</p>
         <span class="text-text-secondary text-sm font-semibold">${date}</span>
       </div>
     </div>
