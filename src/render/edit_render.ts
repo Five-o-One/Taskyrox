5cd998e716d38819f65c03fe3a23392b71745050
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-07-11 17:40:37 +0330

    feat(edit_task): implement task editing render edit card

---
+++ src/render/edit_render.ts
@@ -0,0 +1,9 @@
+import { Modifycard } from '../components/Card/modify_card'
+import { currentTask } from '../state/task_state'
+
+export function renderEditCard(id: number) {
+  const cardWrapper = document.querySelector(`#taskCard-${id}`)
+  if (!cardWrapper) return
+
+  cardWrapper.outerHTML = Modifycard(currentTask)
+}
