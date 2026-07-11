0122474cc193ebb029cd4ab9051854f954d3432f
Shahryar <dev.shahryar@gmail.com>
2026-07-11 10:59:16 +0330

    fix(REGISTRY.ts): ensure renderTaskList is called after event handlers

--- src/events/REGISTRY.ts
+++ src/events/REGISTRY.ts
@@ -1,3 +1,4 @@
+import { renderTaskList } from '../render/list_render'
 import { clickedOnAddEvent } from './card_event'
 import { navbarMobileEvent } from './menu_event'
 import { themeToggleEvent } from './theme_event'
@@ -6,4 +7,6 @@ export default function registry() {
   clickedOnAddEvent()
   themeToggleEvent()
   navbarMobileEvent()
+  // Don't add any events after this line
+  renderTaskList()
 }
