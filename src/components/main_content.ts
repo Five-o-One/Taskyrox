c5b780c4a1893f0ad759650561777d4605ccdc33
MohammadJavad Karamooz <mohammadjaka83@gmail.com>
2026-07-12 16:28:01 +0330

    fix(scrollbar): remove custom scrollbar class for main content

--- src/components/main_content.ts
+++ src/components/main_content.ts
@@ -9,7 +9,7 @@ import { NavBarProfile } from './NavBar/navbar_profile'
 export function MainContent() {
   const tasks = tasksState
   return /* HTML */ ` <div
-    class="bg-bg custom-scrollbar flex h-full w-full flex-col justify-around overflow-y-auto p-4 md:w-4/5"
+    class="bg-bg flex h-full w-full scrollbar-none flex-col justify-around overflow-y-auto p-4 md:w-4/5"
   >
     <div class="mb-16 block md:hidden">${NavBarProfile()}</div>
     <div id="notDoneTasks" class="w-full p-4">${ListTask(false, tasks)}</div>
