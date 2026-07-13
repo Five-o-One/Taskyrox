77cc183718b9972b3588b1065bb6ef52a3f35ea1
Shahryar <dev.shahryar@gmail.com>
2026-07-13 11:48:55 +0330

    fix(profile image): reduce bottom margin of mobile NavBarProfile for better layout

--- src/components/main_content.ts
+++ src/components/main_content.ts
@@ -11,7 +11,7 @@ export function MainContent() {
   return /* HTML */ ` <div
     class="bg-bg flex h-full w-full scrollbar-none flex-col justify-around overflow-y-auto p-4 md:w-4/5"
   >
-    <div class="mb-16 block md:hidden">${NavBarProfile('mobile')}</div>
+    <div class="mb-4 block md:hidden">${NavBarProfile('mobile')}</div>
     <div data-key="not-done-tasks" class="w-full p-4">
       ${ListTask(false, tasks)}
     </div>
